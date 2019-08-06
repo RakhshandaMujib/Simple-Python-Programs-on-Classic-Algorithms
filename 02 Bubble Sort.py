ab6fb6d3cf6e98243afc4b19c7664065e2cf46a4
Rakhshanda Mujib <47360491+RakhshandaMujib@users.noreply.github.com>
2019-08-06 22:19:02 +0530

    Update 02 Bubble Sort.py

--- 02 Bubble Sort.py
+++ 02 Bubble Sort.py
@@ -1,14 +1,10 @@
 def main():
-	print("Enter the numbers in your list: (Enter any alphabet to stop)\n")
-	list_is = []
-	while True:
-		element = input(" ")
-		if element.lower().isalpha():
-			break
-		else:
-			list_is.append(int(element))
+	#Reading the elements:
+	list_is = input("Enter the numbers in your list (separated by spaces):\n").split()
+	list_is = [int(item) for item in list_is] 
 	print(f"The elements are:\n\t{list_is}")
-
+	
+	#Sorting the list: 
 	for i in range(len(list_is)):
 		if i != len(list_is)-1:
 			print(f"\nPass {i+1}:")
